22e14819e48f61072be1f0ccfd08efabe7fdb833
Gabryel Carreiro Batista <53455060+gabryelbatista@users.noreply.github.com>
2021-12-06 09:41:05 -0300

    Update on the Users microservice for validation and error handling + compatibility with flutter_login repository

--- lib/users.dart
+++ lib/users.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: await_only_futures
+
 import 'dart:convert';
 import 'package:functions_framework/functions_framework.dart';
 import 'package:shelf/shelf.dart';
@@ -12,18 +14,43 @@ Future<Response> users(Request request) async {
   await db.open();
   var users = db.collection('users');
 
-  router.get('/login/<login>', (Request request, String login) async {
+  /* router.get('/login/<login>', (Request request, String login) async {
     List user = await users.find({'login': login}).toList();
 
     return Response.ok(json.encode(user));
+  }); */
+
+  router.post('/login', (Request request) async {
+    String payload = await request.readAsString();
+    if (payload.isNotEmpty) {
+      Map<String, dynamic> user = json.decode(payload);
+      Stream<Map<String, dynamic>> response = await users.find(user);
+      if (await response.isEmpty) {
+        return Response.notFound(null);
+      } else {
+        return Response.ok(json.encode(user));
+      }
+    } else {
+      return Response.notFound(null);
+    }
   });
 
   router.post('/create', (Request request) async {
-    final payload = await request.readAsString();
-    Map<String, dynamic> user = json.decode(payload);
-    await users.insert(user);
+    String payload = await request.readAsString();
+    if (payload.isNotEmpty) {
+      Map<String, dynamic> user = json.decode(payload);
+      Stream<Map<String, dynamic>> response =
+          await users.find({"username": user["username"]});
+      if (await response.isEmpty) {
+        await users.insert(user);
 
-    return Response.ok(json.encode(user));
+        return Response.ok(json.encode(user));
+      } else {
+        return Response.notFound(null);
+      }
+    } else {
+      return Response.notFound(null);
+    }
   });
 
   return router(request);
